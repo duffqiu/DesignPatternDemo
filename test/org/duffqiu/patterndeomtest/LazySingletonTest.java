2709ad23be5fd3d98d42ac09cd29b709df9144f7
Duff Qiu <duffqiu@qq.com>
2014-02-05 17:12:43 +0800

    add a test for multi-thread

--- test/org/duffqiu/patterndeomtest/LazySingletonTest.java
+++ test/org/duffqiu/patterndeomtest/LazySingletonTest.java
@@ -5,6 +5,14 @@ package org.duffqiu.patterndeomtest;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import org.duffqiu.patterndemo.LazySingleton;
 import org.junit.Test;
 
@@ -25,4 +33,39 @@ public class LazySingletonTest {
 
     }
 
+    @Test
+    public void testMultithread() throws InterruptedException {
+
+	int threadNum = Runtime.getRuntime().availableProcessors() + 1;
+
+	final List<Object> litSingleton = new Vector<>();
+
+	ExecutorService manager = Executors.newScheduledThreadPool(threadNum);
+
+	Collection<Callable<Object>> tasks = new Vector<Callable<Object>>();
+
+	for (int i = 0; i < threadNum; i++) {
+	    tasks.add(new Callable<Object>() {
+
+		@Override
+		public Object call() throws Exception {
+		    LazySingleton a = LazySingleton.getInstance();
+		    litSingleton.add(a);
+		    return null;
+		}
+
+	    });
+	}
+
+	manager.invokeAll(tasks);
+
+	manager.awaitTermination(2, TimeUnit.SECONDS);
+
+	LazySingleton b = (LazySingleton) litSingleton.get(0);
+
+	for (Object o : litSingleton.toArray()) {
+	    assertEquals(b, o);
+	}
+
+    }
 }
