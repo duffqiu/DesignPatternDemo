e149ec6c171259272e9e54a1f919db764c1ee54f
Duff Qiu <duffqiu@qq.com>
2014-02-22 16:24:21 +0800

    add send message interface

---
+++ src/main/java/org/duffqiu/patterndemo/decorator/ISendMessage.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.duffqiu.patterndemo.decorator;
+
+/**
+ * @author macbook
+ * 
+ *         2014年2月22日
+ */
+public interface ISendMessage {
+
+    int sendMessage(String receiver, int receiverType, String msg);
+
+}
